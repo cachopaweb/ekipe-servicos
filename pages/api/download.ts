7c8db11261c04d9bb4d3a5def3b3dbbaa7942996
cachopaweb <cachopaweb@gmail.com>
2024-01-30 15:12:59 -0400

    correção na api de downlod de arquivos

---
+++ pages/api/download.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import * as path from 'path';
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method !== 'GET') {
+    return response.status(405).end();
+  }
+
+  response.setHeader('Content-Disposition', 'attachment; filename=logo.png');
+  response.setHeader('Content-Type', 'image/png');
+
+  const filePath = path.join(process.cwd(), 'assets', 'logo.png');
+  const fileStream = fs.createReadStream(filePath);
+
+  fileStream.pipe(response);
+
+  response.on('finish', () => {
+    fileStream.close();
+  });
+}
--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import ftp from "basic-ftp";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(){
-    // const client = new ftp.Client()
-    // client.ftp.verbose = true;
-    return NextResponse.json({message: 'enviado com sucesso'})
-}

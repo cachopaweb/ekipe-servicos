f2127c40fe22f18c7a99b7c41f67c1eb178f40b3
Alessandro Dutra de Andrade (cachopaweb) <46660852+cachopaweb@users.noreply.github.com>
2024-09-27 15:57:44 -0400

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const api = axios.create({ baseURL: 'https://servidor-ekipe-fboxwqyjfq-rj.a.run.app/v1' })
-const api = axios.create({  baseURL: 'https://servidor-ekipe-teste-fboxwqyjfq-rj.a.run.app/v1' })
+const api = axios.create({ baseURL: 'https://servidor-ekipe-fboxwqyjfq-rj.a.run.app/v1' })
+// const api = axios.create({  baseURL: 'https://servidor-ekipe-teste-fboxwqyjfq-rj.a.run.app/v1' })
 
 export default api;

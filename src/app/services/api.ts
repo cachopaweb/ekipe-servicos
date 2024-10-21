bcd392bc2a465172b02f14fea9190137044cbb00
cachopaweb <cachopaweb@gmail.com>
2024-10-21 11:09:02 -0400

    add variaveis de ambiente

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: 'https://servidor-ekipe-fboxwqyjfq-rj.a.run.app/v1' })
+const BaseURL = process.env.BASE_URL;
+const api = axios.create({ baseURL: BaseURL })
 // const api = axios.create({  baseURL: 'https://servidor-ekipe-teste-fboxwqyjfq-rj.a.run.app/v1' })
 
 export default api;

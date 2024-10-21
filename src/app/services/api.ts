4f50a9b9b64776d41cdef5eb2c5d2ed5cb593909
iagosauer <iago_ortiz@hotmail.com>
2024-10-21 14:16:03 -0400

    configurando a variavel de ambiente

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseURL = process.env.BASE_URL;
+const BaseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const api = axios.create({ baseURL: BaseURL })
 // const api = axios.create({  baseURL: 'https://servidor-ekipe-teste-fboxwqyjfq-rj.a.run.app/v1' })
 

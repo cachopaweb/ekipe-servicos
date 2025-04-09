a0e37a035e571c1355e58c112ea6d7292f8fb638
iagosauer <iago_ortiz@hotmail.com>
2025-04-09 09:05:38 -0400

    tirando os erros

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,18 +19,6 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin="" />
-
-
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-          crossOrigin=""></script>
-
-          <style>{`
-          #map { height: 400px; }
-          `}</style>
       </head>
       <body className={inter.className}>{
         <AppProvider>
